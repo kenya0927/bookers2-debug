8e56b887156802a4dc9db830270348cc1a7d79d8
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-02-26 08:01:42 +0000

    [Finish]1-3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 
    belongs_to :user
    has_one_attached :image
+   has_many :book_comments,dependent: :destroy
    has_many :favorites,dependent: :destroy
 
   validates :title,presence:true
