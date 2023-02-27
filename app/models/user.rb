6b1270405e97c2b4e6c2bfe048df68c7d81c73a7
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-02-27 08:59:26 +0000

    [Start]1-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   has_many :books
   has_many :book_comments,dependent: :destroy
   has_many :favorites,dependent: :destroy
@@ -16,25 +17,26 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50
-
-
-  def follow(user_id)
-  relationships.create(followed_id: user_id)
-end
-
-def unfollow(user_id)
-  relationships.find_by(followed_id: user_id).destroy
-end
-
-def following?(user)
-  followings.include?(user)
-end
-
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
+  def follow(other_user)
+    self.following_relationships.create(following_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    self.following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
 
 end
