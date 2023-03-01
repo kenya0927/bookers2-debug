08b4e46350ab382c775cdfff9026f2459596c625
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-03-01 08:54:12 +0000

    [Finish]1-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
+  get "search"=>"searches#search"
+
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
    resources :book_comments, only: [:create,:destroy]
    resource :favorites, only: [:create, :destroy]
-end
+  end
 
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
